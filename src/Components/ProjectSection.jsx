900a9401b5507410a257766a852f275f7c3d7f23
vipinKumar-70 <vk20140074250@gmail.com>
2024-11-29 23:43:39 +0530

    initial file add

--- src/Components/ProjectSection.jsx
+++ src/Components/ProjectSection.jsx
@@ -2,18 +2,21 @@ import React, { useRef } from "react";
 import portfolio from "../assets/portfolio.png";
 import cohere from "../assets/cohere.png";
 import netflix from "../assets/netflix.png";
+import wizardz from "../assets/wizardz.png";
 import { Element } from "react-scroll";
 import { useScrollReveal } from "../GSAPManager";
 
 function ProjectCard({ projectImg, linkRef }) {
   return (
-    <div className="col-span-1 overflow-hidden rounded-2xl relative group">
+    <div className="col-span-1 relative group w-full h-[300px] lg:h-[350px] overflow-hidden rounded-2xl">
+      {/* Image */}
       <img
-        className="w-full group-hover:scale-105 duration-300 ease-in-out"
+        className="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
         src={projectImg}
         alt="Project Thumbnail"
       />
-      <div className="bg-[#8844c776] w-full h-full rounded-2xl p-2 absolute z-10 bottom-0 opacity-0 group-hover:opacity-100 duration-300 ease-in grid place-content-center backdrop-blur-md">
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-[#8844c776] rounded-2xl p-2 z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in grid place-content-center backdrop-blur-md">
         <a href={linkRef} target="_blank" rel="noopener noreferrer">
           <button className="md:text-2xl text-xl text-gray-700 font-bold md:p-3 p-1 md:px-5 px-2 bg-gray-300 rounded-2xl">
             Preview
@@ -29,10 +32,9 @@ function ProjectSection() {
   const projectBoxes = useRef([]);
 
   const projectData = [
-    { img: portfolio, link: "/" },
+    { img: cohere, link: "https://cohere-clone.vercel.app/" },
+    { img: wizardz, link: "https://gsap-landing-page-three.vercel.app/" },
     { img: netflix, link: "https://tailwind-netflix-clone.vercel.app/" },
-    { img: cohere, link: "https://cohere-clone.vercel.app/" },
-    { img: cohere, link: "https://cohere-clone.vercel.app/" },
   ];
 
   projectBoxes.current = projectData.map(
@@ -58,6 +60,7 @@ function ProjectSection() {
             className="max-w-[1370px] w-full grid place-items-center gap-y-3"
             ref={projectRef}
           >
+            {/* Header */}
             <div className="lg:text-5xl md:text-4xl text-3xl py-4 font-bold bg-gradient-to-r from-[#8c51e4] via-[#b495e4] to-[#c1a8e7] bg-clip-text text-transparent">
               Recent Projects
             </div>
@@ -65,7 +68,8 @@ function ProjectSection() {
               We put your ideas and thus your wishes in the form of a unique web
               project that inspires you and your customers.
             </div>
-            <div className="grid place-items-center grid-cols-1 md:grid-cols-2 my-5 gap-6 px-10 overflow-hidden">
+            {/* Projects Grid */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-5 px-10">
               {projectData.map((project, index) => (
                 <ProjectCard
                   key={index}
